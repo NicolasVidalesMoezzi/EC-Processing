5766ffb62f4ac5233d5f26904fa4c23117dd9250
NicolasVidalesMoezzi <nvid@outlook.es>
2024-10-15 19:25:15 +0200

    V1.1 Correcion de errores

--- src/p_procesing/p_procesing.ino
+++ src/p_procesing/p_procesing.ino
@@ -1,18 +1,26 @@
-int switchPin = 0;  // Switch connected to pin 4
+//int switchPin = 0;  // Switch connected to pin 4
 
 uint8_t uno = 1;
 uint8_t cero = 0;
 
+float val;
+
 void setup() {
-  pinMode(switchPin, INPUT);  // Set pin 0 as an input
-  Serial.begin(9600);         // Start serial communication at 9600 bps
+  //pinMode(switchPin, INPUT);  // Set pin 0 as an input
+  pinMode(A0, INPUT);
+  Serial.begin(2000000);         // Start serial communication at 9600 bps
 }
 
 void loop() {
-  if (digitalRead(switchPin) == HIGH) {  // If switch is ON,
+ 
+
+  val = analogRead(A0);
+  //Serial.println(val);
+  
+ if (val>3) {  // If switch is ON,
     Serial.write(uno);                     // send 1 to Processing
   } else {                               // If the switch is not ON,
     Serial.write(cero);                     // send 0 to Processing
   }
-  delay(100);                            // Wait 100 milliseconds
+                            // Wait 100 milliseconds
 }
